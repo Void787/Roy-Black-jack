edc116079136ea5bd572ecc21944eecdeb13fb9e
Void787 <1195841@student.roc-nijmegen.nl>
2023-03-27 12:12:59 +0200

    Add project files.

---
+++ Black-jack/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Black_jack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
